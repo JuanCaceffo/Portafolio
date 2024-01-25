827a11d43921acde8be1f29bee8d14618f34bad1
JuanCaceffo <caceffojuan@gmail.com>
2024-01-25 11:47:40 -0300

    eliminado de solouling presunto 3paquage que traducia en runetime

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,9 @@
-import { LOCALE_ID, ApplicationConfig } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { LocaleService } from '@soluling/angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    LocaleService,
-    {
-      provide: LOCALE_ID,
-      deps: [LocaleService],
-      useFactory: (service: LocaleService) => service.localeId,
-    },
   ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,7 @@
 /// <reference types="@angular/localize" />
 
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { loadTranslations } from '@angular/localize';
-import { getTranslations } from '@soluling/angular';
-import { registerLocaleData } from '@angular/common';
-import es from '@angular/common/locales/es'; 
-import en from '@angular/common/locales/en'; 
+import { bootstrapApplication } from '@angular/platform-browser'
+import { appConfig } from './app/app.config'
+import { AppComponent } from './app/app.component'
 
-registerLocaleData(es, 'es'); 
-registerLocaleData(en, 'en'); 
-
-getTranslations('assets/i18n').then((translations) => {
-  if (translations) loadTranslations(translations);
-
-  bootstrapApplication(AppComponent, appConfig).catch((err) =>
-    console.error(err)
-  );
-});
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err))
