eb4302b15383d5cfde5e00c8538fa2874383b7aa
JuanCaceffo <caceffojuan@gmail.com>
2024-01-25 15:26:23 -0300

    cambio de estilado y cerrado de menu mobile al elegir un idioma

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,49 +1,56 @@
-import { Component, DestroyRef, OnInit, inject } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { BurgerButtonComponent } from '../burger-button/burger-button.component';
-import { CommonModule } from '@angular/common';
-import { TranslocoDirective, TranslocoService, translate } from '@ngneat/transloco'
-
+import { Component, DestroyRef, OnInit, inject } from '@angular/core'
+import { RouterLink, RouterLinkActive } from '@angular/router'
+import { BurgerButtonComponent } from '../burger-button/burger-button.component'
+import { CommonModule } from '@angular/common'
+import {
+  TranslocoDirective,
+  TranslocoService,
+  translate,
+} from '@ngneat/transloco'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports:[CommonModule, RouterLink, RouterLinkActive, BurgerButtonComponent, TranslocoDirective],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    BurgerButtonComponent,
+    TranslocoDirective,
+  ],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private translocoService: TranslocoService) {}
 
-  constructor(private translocoService: TranslocoService) { }
-
-  ngOnInit() {
-  }
-  
+  ngOnInit() {}
 
   isActive = false
   navInfo = [
     {
       langKey: 'about',
-      link: 'about'
+      link: 'about',
     },
     {
       langKey: 'project',
-      link: 'projects'
+      link: 'projects',
     },
     {
       langKey: 'experience',
-      link: 'experience'
+      link: 'experience',
     },
     {
       langKey: 'contact',
-      link: 'contact'
-    }
+      link: 'contact',
+    },
   ]
 
   onClicked = () => {
-    this.isActive = !this.isActive 
+    this.isActive = !this.isActive
   }
-  changeLanguague = (lang: 'es'|'en') => {
+  changeLanguague = (lang: 'es' | 'en') => {
     this.translocoService.setActiveLang(lang)
+    this.onClicked()
   }
 }
