18d4bfecb74b737b69d071e0c2263de6f921777e
JuanCaceffo <caceffojuan@gmail.com>
2024-01-27 16:12:04 -0300

    agregado de mokeo de datos que podrian venir del furuto back

---
+++ src/app/data/mocks/card-project-mock.ts
@@ -0,0 +1,13 @@
+export const projectCardData: CardProjectDTO[] = [
+  {
+    pjtType: { es: 'Aplicacion web', en: 'Web App' },
+    title: { es: 'WorldCapp 2023', en: 'WorldCapp 2023' },
+    content: {
+      description: {
+        es: 'worldcapp-2023 es una app de album de figuritas desrrollada con el framework Angular 16 y typescrip. Realizada con muy buenas practicas utilizando el patron de diseño arquitecutral MVC permitiendo una gran escalabilidad del proyecto',
+        en: 'worldcapp-2023 is a sticker album app developed with the Angular 16 framework and TypeScript. It has been implemented with excellent practices using the MVC architectural design pattern, allowing for great scalability of the project.',
+      },
+    },
+    img: '/assets/mocks-imgs/pjt/wordlCapp.jpeg',
+  },
+] 
