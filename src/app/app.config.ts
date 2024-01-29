edba587a561ff6a14c96bb2f2f9c8bd0898fe3b6
JuanCaceffo <caceffojuan@gmail.com>
2024-01-29 15:53:51 -0300

    exportacion de providers para tests

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,20 +7,21 @@ import { provideTransloco } from '@ngneat/transloco'
 
 export type langAvilableTypes = 'en'|'es' 
 const langs = ['en', 'es']
+export const providers = [
+  provideRouter(routes),
+  provideHttpClient(),
+  provideTransloco({
+    config: {
+      availableLangs: langs,
+      defaultLang: 'es',
+      // Remove this option if your application doesn't support changing language in runtime.
+      reRenderOnLangChange: true,
+      prodMode: !isDevMode(),
+    },
+    loader: TranslocoHttpLoader,
+  }),
+]
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-    provideTransloco({
-      config: {
-        availableLangs: langs,
-        defaultLang: 'es',
-        // Remove this option if your application doesn't support changing language in runtime.
-        reRenderOnLangChange: true,
-        prodMode: !isDevMode(),
-      },
-      loader: TranslocoHttpLoader,
-    }),
-  ],
+  providers: providers,
 }
