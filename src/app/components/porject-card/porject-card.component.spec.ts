4e19f2afeeb63aef8b47e0379d8f8f50cb99b215
JuanCaceffo <caceffojuan@gmail.com>
2024-01-30 11:50:18 -0300

    agregado de test

--- src/app/components/porject-card/porject-card.component.spec.ts
+++ src/app/components/porject-card/porject-card.component.spec.ts
@@ -2,17 +2,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { PorjectCardComponent } from './porject-card.component'
-import { AppComponent } from '../../app.component'
 import { CommonModule } from '@angular/common'
-import { provideTransloco, TranslocoDirective } from '@ngneat/transloco'
-import { isDevMode } from '@angular/core'
-import { TranslocoHttpLoader } from '../../data/services/transloco-loader'
-import { provideHttpClient } from '@angular/common/http'
+import { TranslocoDirective } from '@ngneat/transloco'
 import { providers } from '../../app.config'
+import { projectCardData } from '../../data/mocks/card-project-mock'
+import { TestingHelper } from '../../utils/TestingHelper'
 
 describe('PorjectCardComponent', () => {
   let component: PorjectCardComponent
   let fixture: ComponentFixture<PorjectCardComponent>
+  let testingHelper: TestingHelper<PorjectCardComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -24,10 +23,35 @@ describe('PorjectCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PorjectCardComponent)
     component = fixture.componentInstance
+    testingHelper = TestingHelper.createHelper(fixture)
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('A project card component without repo and deploy data do not show these elements', () => {
+    component.cardData = projectCardData[0]
+    fixture.detectChanges()
+    expect(testingHelper.getByTestId('pjt-link-repo')).toBeFalsy()
+    expect(testingHelper.getByTestId('pjt-link-deploy')).toBeFalsy()
+  })
+
+  it('When the project card contains repo and deploy data, these elements will be shown in the window.', () => {
+    component.cardData = {
+      pjtType: { es: '', en: '' },
+      title: { es: '', en: '' },
+      content: {
+        description: {
+          es: '',
+          en: '',
+        },
+        deploy: 'contenido',
+        repo: 'contenido',
+      },
+      img: '',
+    }
+    fixture.detectChanges()
   })
 })
