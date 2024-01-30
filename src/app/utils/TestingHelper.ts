207b6decc6608d8ed2680e129cad0a63f58a8fa7
JuanCaceffo <caceffojuan@gmail.com>
2024-01-30 11:48:15 -0300

    agregado de utils para testing

---
+++ src/app/utils/TestingHelper.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture } from '@angular/core/testing'
+import { NumberSymbol, getLocaleNumberSymbol } from '@angular/common'
+
+export class TestingHelper<T> {
+
+  private constructor(
+    private fixture: ComponentFixture<T>
+  ) {}
+
+  static createHelper<T>(fixture: ComponentFixture<T>) {
+    const testingHelper = new TestingHelper(fixture)
+
+    return testingHelper
+  }
+
+  getByTestId(testId: string) {
+    const resultHtml = this.fixture.debugElement.nativeElement
+    return resultHtml.querySelector(`[data-testid="${testId}"]`)
+  }
+
+  getAllByTestId(testId: string) {
+    const resultHtml = this.fixture.debugElement.nativeElement
+    return resultHtml.querySelectorAll(`[data-testid="${testId}"]`)
+  }
+}
