6a68b0b055090697ad2aac263ba5be4a9961ded0
JuanCaceffo <caceffojuan@gmail.com>
2024-02-03 14:14:01 -0300

    begin layout of the skill bubles

---
+++ src/app/components/skills/skills.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss']
+})
+export class SkillsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslocoDirective } from '@ngneat/transloco';
+import { SkillsComponent } from '../../components/skills/skills.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports:[TranslocoDirective],
+  imports:[TranslocoDirective, SkillsComponent],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
