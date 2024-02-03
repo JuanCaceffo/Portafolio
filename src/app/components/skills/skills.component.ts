24633ec6b45cca2912b0705ef8f7d65bd3455404
JuanCaceffo <caceffojuan@gmail.com>
2024-02-03 14:41:32 -0300

    agregado de traduccion y header

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { TranslocoDirective } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslocoDirective],
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
