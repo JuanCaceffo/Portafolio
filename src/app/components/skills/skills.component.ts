aa08e273e6fc6e78993170de4d43c61e5a2f685f
JuanCaceffo <caceffojuan@gmail.com>
2024-02-03 15:59:55 -0300

    finalizacion de conexion con service y metodo para dividir el array de skills en sub arrays

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,6 +1,8 @@
+import { SkillsService } from './../../data/services/skills.service';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { TranslocoDirective } from '@ngneat/transloco';
+import { SkilllsDTO } from '../../data/interfaces/SkillsDTO';
 
 @Component({
   selector: 'app-skills',
@@ -11,9 +13,29 @@ import { TranslocoDirective } from '@ngneat/transloco';
 })
 export class SkillsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private skillsService: SkillsService) { }
 
   ngOnInit() {
+    this.sliceSkillList()
   }
+  skillLists: Array<SkilllsDTO[]> = []
 
+  sliceSkillList() {
+    const firstColumnSize = 5
+    this.skillsService.getAll().then(value => {
+      const numList = Array.from({ length: (value.length-1) - 0 + 1 }, (_, index) => 0 + index)
+      
+      numList.forEach(num => {
+        console.log(num)
+        const actualColumnSize = firstColumnSize+num
+        if (actualColumnSize - value.length > 0){
+          this.skillLists.push(value.splice(0,actualColumnSize))
+        }else{
+          this.skillLists.push(value)
+          return
+        }
+      })
+    })
+  }
+  
 }
