f55da8e7771b3268559765d2cfb02798f75c04d5
JuanCaceffo <caceffojuan@gmail.com>
2024-02-04 16:07:55 -0300

    refactor slicing method + styled component

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,19 +1,20 @@
-import { SkillsService } from './../../data/services/skills.service';
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { TranslocoDirective } from '@ngneat/transloco';
-import { SkilllsDTO } from '../../data/interfaces/SkillsDTO';
+import { SkillsService } from './../../data/services/skills.service'
+import { CommonModule } from '@angular/common'
+import { Component, OnInit } from '@angular/core'
+import { TranslocoDirective } from '@ngneat/transloco'
+import { SkilllsDTO } from '../../data/interfaces/SkillsDTO'
+import { skillsList } from '../../data/mocks/skills-mock'
+import { from } from 'rxjs'
 
 @Component({
   selector: 'app-skills',
   standalone: true,
   imports: [CommonModule, TranslocoDirective],
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent implements OnInit {
-
-  constructor(private skillsService: SkillsService) { }
+  constructor(private skillsService: SkillsService) {}
 
   ngOnInit() {
     this.sliceSkillList()
@@ -21,21 +22,16 @@ export class SkillsComponent implements OnInit {
   skillLists: Array<SkilllsDTO[]> = []
 
   sliceSkillList() {
-    const firstColumnSize = 5
-    this.skillsService.getAll().then(value => {
-      const numList = Array.from({ length: (value.length-1) - 0 + 1 }, (_, index) => 0 + index)
-      
-      numList.forEach(num => {
-        console.log(num)
-        const actualColumnSize = firstColumnSize+num
-        if (actualColumnSize - value.length > 0){
-          this.skillLists.push(value.splice(0,actualColumnSize))
-        }else{
-          this.skillLists.push(value)
-          return
-        }
-      })
+    const firstColumnSize = 4
+    var num = 0
+    this.skillsService.getAll().then((value) => {
+      const copyValue = Array.from(value)
+      while ((copyValue.length - (firstColumnSize + num)) > 0) {
+        this.skillLists.push(copyValue.splice(0, firstColumnSize + num))
+        num++
+      }
+      this.skillLists.push(copyValue)
     })
+    console.log(skillsList)
   }
-  
 }
