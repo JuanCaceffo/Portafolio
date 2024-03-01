9f902d19680d7de6b02b479f42e753c02469bd56
JuanCaceffo <caceffojuan@gmail.com>
2024-03-01 16:37:04 -0300

    maquetado e estilado inicial para mobile

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
 
@@ -12,4 +16,14 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
   }
 
+  inputPros = [
+    {
+      type: 'text',
+      pleaceholderKey: "name"
+    },
+    {
+      type: 'email',
+      pleaceholderKey: "email"
+    }
+  ]
 }
