abc88cd191a574fb52e43358ec78efad447882a1
JuanCaceffo <caceffojuan@gmail.com>
2024-03-01 22:44:42 -0300

    agregado de estilado para pc

--- src/app/data/services/config.ts
+++ src/app/data/services/config.ts
@@ -1,5 +1,5 @@
 //TODO: pass this to an environment variables
-export const pathApi = 'http://localhost:8080'
+export const pathApi = 'http://192.168.100.4:8080'
 export const paths = {
   project: 'project',
   info: 'info'
