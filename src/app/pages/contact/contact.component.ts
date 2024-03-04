c573209fd5c54211016d3768e8e0eeaf1a36eb6e
JuanCaceffo <caceffojuan@gmail.com>
2024-03-04 15:41:13 -0300

    implementacion de de inputs de angular material

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,17 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TranslocoDirective } from '@ngneat/transloco';
 import { ContactDTO } from '../../data/interfaces/ContactDTO';
 import { PersonalInfoService } from '../../data/services/PersonalInfo.service';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, TranslocoDirective,FormsModule],
+  imports: [CommonModule, TranslocoDirective,FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ContactComponent {
 
