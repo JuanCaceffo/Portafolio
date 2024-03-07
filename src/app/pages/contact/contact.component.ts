8e3718f681567cd74db65b9e04407d1db10eb1a1
JuanCaceffo <caceffojuan@gmail.com>
2024-03-07 13:30:27 -0300

    implementacion snackbars al enviar correo

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,25 +1,42 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { TranslocoDirective } from '@ngneat/transloco';
-import { ContactDTO } from '../../data/interfaces/ContactDTO';
-import { PersonalInfoService } from '../../data/services/PersonalInfo.service';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { SnackbarAlertService } from './../../data/services/snackbar-alert.service';
+import { CommonModule } from '@angular/common'
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { TranslocoDirective, TranslocoService } from '@ngneat/transloco'
+import { ContactDTO } from '../../data/interfaces/ContactDTO'
+import { PersonalInfoService } from '../../data/services/PersonalInfo.service'
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatSnackBarRef, TextOnlySnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, TranslocoDirective,FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    CommonModule,
+    TranslocoDirective,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
-export class ContactComponent {
+export class ContactComponent implements OnDestroy{
+  constructor(
+    private infoService: PersonalInfoService,
+    private snackBar: SnackbarAlertService,
+    private translocoService: TranslocoService
+  ) {}
 
-  constructor(private infoService: PersonalInfoService) { }
-  emptyContactData = {name: '', email:'',message:''}
+  ngOnDestroy() {
+    this.snackRef && this.snackRef.dismiss()
+  }
+
+  emptyContactData = { name: '', email: '', message: '' }
   data: ContactDTO = Object.assign({}, this.emptyContactData)
+  snackRef?: MatSnackBarRef<TextOnlySnackBar> 
 
   inputPros = [
     {
@@ -29,13 +46,18 @@ export class ContactComponent {
     {
       key: 'email' as keyof ContactDTO,
       type: 'email',
-    }
+    },
   ]
 
-  isNotComplete = () => (Object.values(this.data).some(value => !value))
+  isNotComplete = () => Object.values(this.data).some((value) => !value)
 
-  sendMessage(){
-    this.infoService.contact(this.data)
+  sendMessage() {
+    this.infoService.contact(this.data).then((_value) => {
+      this.snackBar.openSuccesSnack({message: this.translocoService.translate('contact.email-succes'), action: '', config: {  verticalPosition:'top'}})
+    }
+    ).catch((_err) => {
+      this.snackRef= this.snackBar.openErrorSnack({message: this.translocoService.translate('contact.email-error'), action: '', config: {  verticalPosition:'top'}})
+    })
     this.data = Object.assign({}, this.emptyContactData)
   }
 }
