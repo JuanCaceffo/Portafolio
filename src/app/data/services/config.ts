e29ef118aa328d7eadc9f96243586db4d8c993e6
JuanCaceffo <caceffojuan@gmail.com>
2024-03-09 12:17:54 -0300

    added api path for production

--- src/app/data/services/config.ts
+++ src/app/data/services/config.ts
@@ -1,5 +1,5 @@
 //TODO: pass this to an environment variables
-export const pathApi = 'http://192.168.100.4:8080'
+export const pathApi = 'https://api-portafolio-xjbf.onrender.com'
 export const paths = {
   project: 'project',
   info: 'info'
